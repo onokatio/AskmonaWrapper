f22cb695aa16e2f954c91145b313a1a0ee290d09
onokatio <onokatio@gmail.com>
2016-12-18 14:24:27 +0900

    fix http and add topics

--- httprequest.php
+++ httprequest.php
@@ -1,43 +1,49 @@
 <?php
-
+//				'Content-Type: application/x-www-form-urlencoded',
 class Askmona{
+	var $arraynum = 0; // 0:make0 1:dont make 0
+	
 	private function http($url,$post=""){
-		if($post){
-			$header = array(
-				'Content-Type: application/x-www-form-urlencoded',
-				'Content-Length: '.strlen($post),
-			);
-			$option = array('http' => array(
-				'ignore_errors'=> true,
-				'method' => 'POST',
-				'header' => implode("\r\n",$header),
-				'content' => $post,
-			));
-			if( ! ( $json = file_get_contents($url,false,stream_context_create($option)))) return 0;
-		}else{
-			$option = stream_context_create(array('http'=>array('ignore_errors'=>true)));
-			if( ! ( $json = file_get_contents($url,false,$option))) return 0;
-		}
-		return json_decode($json);
+//		$header = array('Content-Length: '.strlen($post));
+		$option = array('http' => array(
+			'ignore_errors'=> true,
+			'method' => 'POST',
+//			'header' => implode("\r\n",$header),
+			'header' => 'Content-Length: '.strlen($post),
+			'content' => $post,
+		));
+		$json = file_get_contents($url,false,stream_context_create($option));
+		$return = json_decode($json);
+		if(!$return->status) throw new Exception("error: {$return->error}");
+		return $return;
+	}
+
+
+
+	function topics($limit=0,$offset=0) {
+		if(!$limit) $limit=25;
+		$return = $this->http("https://askmona.org/v1/topics/list?limit={$limit}&offset={$offset}");
+		if(count($return->topics)===1) $return->topics = $return->topics[0];
+		return $return;
 	}
 	
 	function res($t_id, $from, $to=0) {
 		if(!$to) $to=$from;
-		try{
-			$return = $this->http("https://askmona.org/v1/responses/list?t_id=${t_id}&from={$from}&to={$to}&topic_detail=1");
-		}catch(Exception $e){
-			echo $e;
-		}
-		if(!$return->status) throw new Exception("error: {$return->error}");
+		$return = $this->http("https://askmona.org/v1/responses/list?t_id=${t_id}&from={$from}&to={$to}");
+		if(count($return->responses)===1) $return->responses = $return->responses[0];
 		return $return;
 	}
 	
-	function topics() {
-		return $this->http('https://askmona.org/v1/topics/list');
+	function user($u_id) {
+		return $this->http("https://askmona.org/v1/users/profile?u_id={$u_id}");
 	}
 }
+
+
 	$askmona = new Askmona();
-	$test = $askmona->res(3442,1);
-	echo $test->responses[0]->u_name;
-	$test = $askmona->res(9999,1);
+	echo $askmona->res(3442,1)->responses->u_name."\n";
+	echo $askmona->user(1)->u_name."\n";
+	echo $askmona->topics(1)->topics->title;
 ?>
+
+
