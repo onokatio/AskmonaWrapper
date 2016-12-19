e270e91df1d5e8584fe6161b65da5eae339a97b4
onokatio <onokatio@gmail.com>
2016-12-19 19:11:28 +0900

    change name

--- askmona.php
+++ askmona.php
@@ -17,15 +17,14 @@
  #    #  #  # #  #    #  #  #    ######     #       #    #    #
  #    #  #   ##  #    #  #   #   #    #     #       #    #    #
   ####   #    #   ####   #    #  #    #     #       #     ####
-
 */
 class Askmona{
-	public $zero_flags = FALSE; // FALSE:make0 TRUE:dont make 0
+	private $zero_flags = 0; // FALSE:make0 TRUE:dont make 0
 	
 	private function throw_ecp($message) { //throw exception by function
 		throw new Exception($message);
 	}
-	
+		
 	private function http($url,$post=''){ //send http request
 		$option = array('http' => array(
 			'ignore_errors'=> true,
@@ -34,26 +33,26 @@ class Askmona{
 			'content' => $post,
 		));
 		$return = json_decode(file_get_contents($url,false,stream_context_create($option)));
-		!$return->status && throw_ecp("error: {$return->error}");
+		!$return->status && $this->throw_ecp("error: {$return->error}");
 		return $return;
 	}
 
 
-	function topics($limit=0,$offset=0) {
+	public function topics($limit=0,$offset=0) {
 		!$limit && $limit=25;
 		$return = $this->http("https://askmona.org/v1/topics/list?limit={$limit}&offset={$offset}");
-		$this->zero_flags && count($return->topics)===1 && $return->topics = $return->topics[0];
+		!$this->zero_flags && count($return->topics)===1 && $return->topics = $return->topics[0];
 		return $return;
 	}
 	
-	function res($t_id, $from, $to=0) {
+	public function res($t_id, $from, $to=0) {
 		!$to && $to = $from;
 		$return = $this->http("https://askmona.org/v1/responses/list?t_id=${t_id}&from={$from}&to={$to}");
-		$this->zero_flags && count($return->responses)===1 && $return->responses = $return->responses[0];
+		!$this->zero_flags && count($return->responses)===1 && $return->responses = $return->responses[0];
 		return $return;
 	}
 	
-	function user($u_id) {
+	public function user($u_id) {
 		return $this->http("https://askmona.org/v1/users/profile?u_id={$u_id}");
 	}
 }
@@ -63,6 +62,7 @@ class Askmona{
 	echo $askmona->res(3442,1)->responses->u_name."\n";
 	echo $askmona->user(1)->u_name."\n";
 	echo $askmona->topics(1)->topics->title;
+
 ?>
 
 
