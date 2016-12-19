aceb99024927a87701356f54d5bdd1ae147ad31e
onokatio <onokatio@gmail.com>
2016-12-19 19:17:44 +0900

    show topics test

--- demo.php
+++ demo.php
@@ -2,10 +2,8 @@
 require('askmona.php');
 
 
-
 	$askmona = new Askmona();
-	echo $askmona->res(3442,1)->responses->u_name."\n";
-	echo $askmona->user(1)->u_name."\n";
-	echo $askmona->topics(1)->topics->title;
-
+	//echo $askmona->res(3442,1)->responses->u_name."\n";
+	//echo $askmona->user(1)->u_name."\n";
+	for($i=0;$i<10;$i++) printf($askmona->topics(10)->topics[$i]->title."\n");
 ?>
