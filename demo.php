41b5ae4bfbee6ccfc3c7425400885d7433f46750
onokatio <onokatio@gmail.com>
2017-01-29 23:33:41 +0900

    change function system

--- demo.php
+++ demo.php
@@ -2,18 +2,22 @@
 	require('vendor/autoload.php');
 	use onokatio\AskmonaWrapper\Askmona;
 
-	foreach(Askmona::topics(3)->topics as $topic) echo($topic->title."\n");
+	//foreach(Askmona::topics(3)->topics as $topic) echo($topic->title."\n");
+	foreach(Askmona::get('topics/list',array('limit'=>3))->topics as $topic) echo($topic->title."\n");
+	$array = array(
+		't_id'=>1,
+		'from'=>3
+	);
+	foreach(Askmona::get('responses/list',$array)->responses as $response) echo($response->response."\n");
 
-	foreach(Askmona::res(1,1,3)->responses as $response) echo($response->response."\n");
+	echo Askmona::get('users/profile',array('u_id'=>1))->u_name;
 
-	echo(Askmona::user(1)->u_name);
-
-	echo Askmona::user(1)
-	->_
-	->user(1)
-	->_
-	->user(1)->u_name;
-
-	$a = new Askmona();
-	//var_dump($a);
+	$a = new Askmona('UCJn+IU2h2o5JTXfjd5adqgCg8uQHRJOdj9ngfkFOCNc=', 2473, 'A08NRW1v6qTX2zbQnum3EV9EWF0xaexZMJStBskUTmEs=', 2473);
+	var_dump($a);
+	$array = array(
+		't_id'=>3446,
+		'text'=>'without id',
+		'sage'=>1
+	);
+	$a->post('responses/post',$array);
 ?>
--- src/onokatio/AskmonaWrapper/Askmona.php
+++ src/onokatio/AskmonaWrapper/Askmona.php
@@ -18,62 +18,68 @@
  #    #  #   ##  #    #  #   #   #    #     #       #    #    #
   ####   #    #   ####   #    #  #    #     #       #     ####
 
-//*/
+*/
 namespace onokatio\AskmonaWrapper;
 
 define('HOSTNAME','https://askmona.org/v1/');
-define('DEBKEY','input default dev key here.');
+define('DEVKEY','input default developer key here.');
+define('DEVID',0);
+
 
 
 class Askmona{
-	private $zero_flags = 0; // FALSE:make0 TRUE:dont make 0
-	const HOST = HOSTNAME;
-	
-	private $address = '';
-	private $password = '';
 	private $token = '';
-	private $devkey = DEBKEY;
+	private $devkey = DEVKEY;
+	private $id = array(
+		'u_id' => 0,
+		'app_id' => DEVID
+	);
+	
+	public function __construct(string $token, int $u_id, string $devkey = '', int $app_id = 0){
+		$this->token = $token;
+		$this->id['u_id'] = $u_id;
+		empty($devkey) || $this->devkey = $devkey;
+		empty($app_id) || $this->id['app_id'] = $app_id;
+	}
 	
 	private function throw_ecp(string $message) {throw new Exception($message);}  //throw ecp as func
 	
-	public function __construct(string $address = '', string $password = ''){
-		if( empty($password) ){
-			$this->token = $address;
-		}else{
-			$this->address = $address;
-			$this->password = $password;
-		}
+	public function Auth(){
+		$nonce = base64_encode(random_bytes(32));
+		$time = time();
+		return array(
+			'nonce'=>$nonce,
+			'time'=>$time,
+			'auth_key'=>base64_encode(hash('sha256',$this->devkey.$nonce.$time.$this->token,TRUE))
+		);
 	}
 	
-	private function http(string $url, string $post=''){ //send http request
+	public function get(string $url, $query){ //send http request
+		$query = http_build_query($query);
+		$return = file_get_contents(HOSTNAME.$url.'?'.$query);
+		$return = json_decode($return);
+//		$return->status || self::throw_ecp("error: {$return->error}");
+		isset($this) &&  $return->_ = $this;
+		return $return;
+	}
+	
+	public function post(string $url, $query){ //send http request
+		$query = http_build_query(array_merge($query,$this->Auth(),$this->id));
+		$headers = array(
+			'Content-Type: application/x-www-form-urlencoded',
+		);
 		$option = array('http' => array(
 			'ignore_errors'=> true,
 			'method' => 'POST',
-			'header' => 'Content-Length: '.strlen($post),
-			'content' => $post,
+			'header' => 'Content-Length: '.strlen($query),
+			'content' => $query,
+			'header' => implode("\r\n", $headers),
 		));
-		$return = json_decode(file_get_contents($url,NULL,stream_context_create($option)));
-		!$return->status && self::throw_ecp("error: {$return->error}");
-		isset($this) ? $return->_ = $this : $return->_ = new Self;
+		$return = file_get_contents(HOSTNAME.$url,false,stream_context_create($option));
+		$return = json_decode($return);
+//		$return->status || self::throw_ecp("error: {$return->error}");
+		isset($this) && $return->_ = $this;
 		return $return;
-	}
-
-
-	public function topics(int $limit=25,int $offset=0){
-		$return = self::http(self::HOST."topics/list?limit={$limit}&offset={$offset}");
-		isset($this) && $this->zero_flags && count($return->topics)===1 && $return->topics = $return->topics[0];
-		return $return;
-	}
-	
-	public function res(int $t_id, int $from, int $to=0) {
-		!$to && $to = $from;
-		$return = self::http(self::HOST."responses/list?t_id=${t_id}&from={$from}&to={$to}");
-		isset($this) && $this->zero_flags && count($return->responses)===1 && $return->responses = $return->responses[0];
-		return $return;
-	}
-	
-	public function user(int $u_id) {
-		return self::http(self::HOST."users/profile?u_id={$u_id}");
 	}
 }
 ?>
