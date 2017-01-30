01089e0d00ee8f5bf18681838bb14853a014546f
onokatio <onokatio@gmail.com>
2017-01-30 21:59:06 +0900

    edit line and fix

--- demo.php
+++ demo.php
@@ -3,8 +3,12 @@
 	require('src/onokatio/AskmonaWrapper/Askmona.php'); //ロード
 	use onokatio\AskmonaWrapper\Askmona; //namespaceの設定
 
+
+
 	//最新のトピック一覧を1つ取得してみる
 	echo(Askmona::get('topics/list',array('limit'=>3))->topics[0]->title);
+
+
 
 	//こんどはトピックID1の1番めのレスを取得してみる。
 	$array = array(
@@ -13,8 +17,12 @@
 	);
 	echo(Askmona::get('responses/list',$array)->responses[0]->response);
 
+
+
 	//ユーザーID2473のプロフィールを取得する
 	echo Askmona::get('users/profile',array('u_id'=>2473))->u_name;
+
+
 
 	//トピックID3446にsage有効で「てすと」と投稿してみる
 	//今はダミーのID・キーが入ってるのでエラーが起こるはずです。
